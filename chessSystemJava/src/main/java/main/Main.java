3b9f390b7dd09bea947868c0d2a6c05d5bf9977a
samuelccp0161 <samuelccp0161@gmail.com>
2023-08-08 11:11:53 -0300

    First class: Position

--- chessSystemJava/src/main/java/main/Main.java
+++ chessSystemJava/src/main/java/main/Main.java
@@ -1,7 +1,10 @@
 package main;
 
+import boardGame.Position;
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello World!!!");
+        Position position = new Position(3, 5);
+        System.out.println(position);
     }
 }
---
+++ chessSystemJava/src/test/java/boardGame/PositionTest.java
@@ -0,0 +1,15 @@
+package boardGame;
+
+import org.junit.Test;
+
+import static org.junit.Assert.assertEquals;
+
+public class PositionTest {
+    @Test
+    public void testPosition() {
+        Position position = new Position(3,5);
+
+        assertEquals(3, position.row);
+        assertEquals(5, position.column);
+    }
+}
